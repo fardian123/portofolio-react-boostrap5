d4f43a3aaf17534569c061d543d2f3da8f8beb81
fardian123 <fardianlubis@gmail.com>
2024-05-23 19:49:50 +0700

    styling button at projects section

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -40,7 +40,7 @@ function Portfolio() {
                 <div className="card-body">
                   <h5 className="card-title">{project.title}</h5>
                   <p className="card-text">{project.description}</p>
-                  <a href={project.link} className="btn btn-primary">View Project</a>
+                  <a href={project.link} className="btn pj-btn">View Project</a>
                 </div>
               </div>
             </div>
