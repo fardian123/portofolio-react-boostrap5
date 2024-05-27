a65eb08b3a1d0a677f7d1c334669f2c42a2d46a9
fardian123 <fardianlubis@gmail.com>
2024-05-27 09:44:54 +0700

    add typed.js and styling the hero text

--- src/Component/HeroSection.jsx
+++ src/Component/HeroSection.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import imgHero from "../assets/heroBanner.svg";
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Typed from "typed.js";
 
 const HeroSection = () => {
-  useEffect(()=>{
+  useEffect(() => {
     Aos.init();
-  },[])
+  }, []);
+
+  // typed js config
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Fardian Zahri Chaniago."],
+      typeSpeed: 77,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <section className="container hero" id="aboutme">
       <div className="row align-items-center py-5 mt-5 col-reverse">
@@ -15,7 +31,9 @@ const HeroSection = () => {
         <div className="col-md-6">
           <div className="" data-aos="fade-right">
             <p className="hero-intro-text mb-0">Hello i'm</p>
-            <h1 className="display-4 hero-name-text">Fardian Zahri Chaniago</h1>
+            <h1 className="display-4 hero-name-text">
+              <span ref={el}>Fardian Zahri Chaniago </span>{" "}
+            </h1>
             <p className="lead">
               I am a passionate web developer with experience in React, Laravel,
               Bootstrap, and TailwindCSS.
@@ -27,7 +45,13 @@ const HeroSection = () => {
         </div>
         {/* Bagian Kanan - Gambar */}
         <div className="col-md-6 d-flex justify-content-center">
-          <img src={imgHero} width={300} alt="HeroImage" className="heroImg" data-aos="fade-left"/>
+          <img
+            src={imgHero}
+            width={300}
+            alt="HeroImage"
+            className="heroImg"
+            data-aos="fade-left"
+          />
         </div>
       </div>
     </section>
