9787a570986a69a1224db30ffaddca357fdb6bd0
fardian123 <fardianlubis@gmail.com>
2024-05-29 00:37:26 +0700

    update

--- src/Component/Education.jsx
+++ src/Component/Education.jsx
@@ -3,12 +3,11 @@ import React from "react";
 import laravel from "../assets/devpack/laravel.svg";
 import react from "../assets/devpack/react.svg";
 
-import html from "../assets/devpack/html.svg";
-import css from "../assets/devpack/css.svg";
-import js from "../assets/devpack/js.svg";
-
 import pnpm from "../assets/devpack/pnpm.svg";
 import npm from "../assets/devpack/npm.svg";
+
+import git from "../assets/devpack/git.svg";
+import github from "../assets/devpack/github.svg";
 
 import bootstrap from "../assets/devpack/bootstrap.svg";
 import tailwind from "../assets/devpack/tailwind.svg";
@@ -19,7 +18,9 @@ const Education = () => {
       <h1>Skills & Education</h1>
 
       <div className="row d-flex flex-wrap flex-column flex-md-row justify-content-between gap-3">
-        <div className="col-12 col-md-5">
+        <div className="col-12 col-md-5 " data-aos="fade-right"
+              data-aos-duration="500"
+              data-aos-easing="ease-in-sine">
           <div className="card mt-2">
             <div className="card-header">
               <h3 className="pre-title">Learning Path</h3>
@@ -40,15 +41,17 @@ const Education = () => {
                   <div></div>
                 </div>
                 <div className="education-info py-4">
-                  <h4 className="education-title">Universitas Pamulang</h4>
-                  <p>Teknik Informatika</p>
-                  <h4 className="education-years">2022 - Sekarang</h4>
+                  <h4 className="education-title">SMK Nusantara 01 Ciputat</h4>
+                  <p>Rekayasa Perangkat Lunak</p>
+                  <h4 className="education-years">2019 - 2022</h4>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="col-12 col-md-5">
+        <div className="col-12 col-md-5" data-aos="fade-left"
+              data-aos-duration="500"
+              data-aos-easing="ease-in-sine">
           <div className="card d-flex gap-1">
             <div className="card-header">
               <h3> Hard Skills</h3>
@@ -71,22 +74,11 @@ const Education = () => {
                   <img src={npm} alt="" width={40} className="item" />
                 </div>
               </div>
-              <div>
-                <h5>Programming Languages</h5>
-                <div className="d-flex mt-2 gap-2">
-                  <img src={html} alt="" width={40} />
-                  <img src={css} alt="" width={40} />
-                  <img src={js} alt="" width={40} />
-                </div>
-                <div className="d-flex mt-2 gap-2">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg"
-                    width={40}
-                  />
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg"
-                    width={40}
-                  />
+              <div className="px-3">
+                <h5 className="text-wrap">Versions Control System</h5>
+                <div className="d-flex gap-2">
+                  <img src={git} alt="" width={40} />
+                  <img src={github} alt="" width={40} />
                 </div>
               </div>
             </div>
